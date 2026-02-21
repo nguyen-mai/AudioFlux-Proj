26edf5b69f05bd46ba38f1707bf0a79f4885b6d4
mainq <nguyenmai27019@gmail.com>
2026-02-22 00:49:54 +0700

    fix: add header title file mp3 downloaded

--- server/main.py
+++ server/main.py
@@ -123,8 +123,12 @@ def download_mp3(job_id: str):
 
     if not os.path.exists(file_path):
         raise HTTPException(status_code=404, detail="File not found")
+    
+    # 👇 lấy title từ job
+    title = job.get("title", job_id)
 
     return FileResponse(
         path=file_path,
-        media_type="audio/mpeg"
+        media_type="audio/mpeg",
+        filename=f"{title}.mp3"
     )
